7722531630fb233416b280fe79f5cc4fddc1b3d9
Viktor Sermyazhko <VashaAkulka@mail.ru>
2024-03-31 21:57:53 +0300

    yes

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -10,7 +10,7 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    public List<Product> listExecuteFilteringStrategy(FilteringStrategy filteringStrategy) {
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy) {
         return productList.stream().filter(filteringStrategy::filter).collect(Collectors.toList());
     }
 }
